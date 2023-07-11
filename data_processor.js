5576c543559c524a372fa1943210c5cd4ac753bc
yonishay <yoni.shay@gmail.com>
2023-07-12 00:37:23 +0300

    adding try catch to data_processor

--- data_processor.js
+++ data_processor.js
@@ -14,33 +14,45 @@ if (args.length < 1) {
 }
 
 async function processEventFile(fileName) {
-    const fileStream = fs.createReadStream(fileName, 'utf-8');
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity,
-    });
+    try {
+        const fileStream = fs.createReadStream(fileName, 'utf-8');
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity,
+        });
 
-    let batchData = [];
+        let batchData = [];
 
-    for await (const line of rl) {
-        const { userId, name, value } = JSON.parse(line);
-        const revenue = name === 'add_revenue' ? value : -value;
-        const existingData = batchData.find((data) => data.userId === userId);
+        for await (const line of rl) {
+            try {
+                const { userId, name, value } = JSON.parse(line);
+                const revenue = name === 'add_revenue' ? value : -value;
+                const existingData = batchData.find((data) => data.userId === userId);
 
-        if (existingData) {
-            existingData.revenue += revenue;
-        } else {
-            batchData.push({ userId, revenue });
+                if (existingData) {
+                    existingData.revenue += revenue;
+                } else {
+                    batchData.push({ userId, revenue });
+                }
+
+                if (batchData.length >= batchSize) {
+                    await processBatch(batchData);
+                    batchData = [];
+                }
+            } catch (error) {
+                console.error('Error processing line:', line);
+                console.error(error);
+                // Handle or log the error as needed
+            }
         }
 
-        if (batchData.length >= batchSize) {
+        if (batchData.length > 0) {
             await processBatch(batchData);
-            batchData = [];
         }
-    }
-
-    if (batchData.length > 0) {
-        await processBatch(batchData);
+    } catch (error) {
+        console.error('Error reading file:', fileName);
+        console.error(error);
+        // Handle or log the error as needed
     }
 }
 
